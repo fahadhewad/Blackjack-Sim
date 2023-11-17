35f3cc23d708ab7e45d74fdd076617fde1b8e0c8
fahadhewad <fahadhewad@gmail.com>
2023-11-17 04:32:37 +0000

    bunch of bug fixes, added insurance win count, blackjack counter, and fixed shuffling

--- bj.py
+++ bj.py
@@ -8,6 +8,7 @@ userWinCount = 0
 dealerWinCount = 0
 userBJCount = 0
 dealerBJCount = 0
+insuranceWinCount = [0, 0]
 
 def deckMake(): 
     for card in cards:
@@ -18,8 +19,9 @@ def deckMake():
 def dealDeck(n):
     for i in range(n):
         deckMake()
-
-dealDeck(3) # 3 decks made
+    random.shuffle(deck)
+    
+dealDeck(2) # 3 decks made
 
 random.shuffle(deck)
 
@@ -31,11 +33,11 @@ def reshuffle():
     print("Deck out of cards!")
     userInput = int(input("Type the number of decks you want shuffled in. 0 to Exit.\n"))
     if userInput == 0:
-        exit
-    elif dealDeck(userInput):
+        exit()
+    else:
         deck.clear()
         dealDeck(userInput)
-        print("Shuffled " + userInput + " decks.")
+        print("Shuffled " + str(userInput) + " decks.")
 
 def deal():
     if len(deck) < 4:
@@ -50,10 +52,12 @@ def deal():
 def count(player):
     count = 0
     number = 0
+    ace = False
+
     for card in player:
         arr = card.split('-')
         if arr[0] in tens:
-            number = 10 
+            number = 10
             count += number
         elif arr[0] == 'A':
             if count + 11 > 21:
@@ -84,7 +88,7 @@ def checkWinner():
     print(dealer, " = ", dealerCount, " = Dealer")
     print(user, " = ", userCount, " = Player")
     print("---------------")
-    while count(dealer) < 18:
+    while count(dealer) < 17:
         hitPerson(dealer)
         dealerCount = count(dealer)
         userCount = count(user)
@@ -105,7 +109,7 @@ def checkWinner():
         print("Push!")
     else:
         dealerWinCount += 1
-        print("Dealer wins!")
+        print("!")
     reset()
 
 def start():
@@ -128,14 +132,15 @@ def start():
             print(user, " = ", userCount, " = Player")
             print("---------------")
             print("User wins! - BlackJack")
-            reset()
-            deal()
             print("Player wins = " + str(userWinCount) + " | Dealer wins = " + str(dealerWinCount))
             print("Player blackjacks = " + str(userBJCount) + " | Dealer blackjacks = " + str(dealerBJCount))
+            print("Insurance wins and losses w/l " + str(insuranceWinCount[0]) + "|" + str(insuranceWinCount[1]))
+            reset()
+            deal()
             continue
 
         if bjUser and bjDealer:
-            dealerBJCount += 1
+            userBJCount += 1
             dealerBJCount += 1
             dealerCount = count(dealer)
             userCount = count(user)
@@ -143,10 +148,11 @@ def start():
             print(user, " = ", userCount, " = Player")
             print("---------------")
             print("Push! - BlackJack")
-            reset()
-            deal()
             print("Player wins = " + str(userWinCount) + " | Dealer wins = " + str(dealerWinCount))
             print("Player blackjacks = " + str(userBJCount) + " | Dealer blackjacks = " + str(dealerBJCount))
+            print("Insurance wins and losses w/l " + str(insuranceWinCount[0]) + "|" + str(insuranceWinCount[1]))
+            reset()
+            deal()
             continue
 
         firstCard = [dealer[0]]
@@ -156,6 +162,32 @@ def start():
         print("Dealer = " + str(count(firstCardCount)))
         print(user)
         print("Player = " + str(count(user)))
+        if dealer[0][0] == 'A':
+            userInput = int(input("Would you like insurance? 1 = Yes, 0 = No\n"))
+            if userInput == 1:
+                if dealer[1] in tens:
+                    insuranceWinCount[0] += 1
+                else:
+                    insuranceWinCount[1] += 1
+            if userInput == 0:
+                pass
+            
+            if bjDealer: # needs work when dealer gets A A A K, MAKES HIM GO BUST, ALSO FIX THE WIN COUNTS
+                dealerWinCount += 1
+                dealerBJCount += 1
+                dealerCount = count(dealer)
+                userCount = count(user)
+                print(dealer, " = ", dealerCount, " = Dealer")
+                print(user, " = ", userCount, " = Player")
+                print("---------------")
+                print("Dealer wins! - Blackjack")
+                print("Player wins = " + str(userWinCount) + " | Dealer wins = " + str(dealerWinCount))
+                print("Player blackjacks = " + str(userBJCount) + " | Dealer blackjacks = " + str(dealerBJCount))
+                print("Insurance wins and losses w/l " + str(insuranceWinCount[0]) + "|" + str(insuranceWinCount[1]))
+                reset()
+                deal()
+                continue
+
         if count(user) > 21:
             print("----------------------------------------------------------------------")
             print(dealer)
@@ -167,6 +199,7 @@ def start():
             deal()
             print("Player wins = " + str(userWinCount) + " | Dealer wins = " + str(dealerWinCount))
             print("Player blackjacks = " + str(userBJCount) + " | Dealer blackjacks = " + str(dealerBJCount))
+            print("Insurance wins and losses w/l " + str(insuranceWinCount[0]) + "|" + str(insuranceWinCount[1]))
             continue
 
         userInput = int(input("1 = Hit, 0 = Stand\n"))
@@ -179,6 +212,7 @@ def start():
             deal()
             print("Player wins = " + str(userWinCount) + " | Dealer wins = " + str(dealerWinCount))
             print("Player blackjacks = " + str(userBJCount) + " | Dealer blackjacks = " + str(dealerBJCount))
+            print("Insurance wins and losses w/l " + str(insuranceWinCount[0]) + "|" + str(insuranceWinCount[1]))
             continue
     
 start()
